35b3c306828601528e6750711ec5a6dc203fdd07
Sachin Kumar Pal <56400187+sachinpal293@users.noreply.github.com>
2022-07-02 21:24:17 +0530

    Add files via upload

---
+++ javascript 2022/index.js
@@ -0,0 +1,10 @@
+console.log("Hello")
+
+/* Section 3 value and variable in javascript  */
+var a = "sachin Kumar Pal"
+let b = "shubham"
+console.log(a)
+sachin = "Computer"
+console.log(sachin)
+
+console.log("Hello git")
